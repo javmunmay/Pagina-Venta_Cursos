30c84cc756ea73b9ec0d5b5b96ace58be3d6c821
javmunmay <javi15mmj@gmail.com>
2024-10-25 18:51:14 +0200

    Mejoras e inicio de implementación de reducción de código (para que este más limpio) y unificación de footer...  Versión 1.7.4 (BETA)

---
+++ php/footer.php
@@ -0,0 +1,20 @@
+<footer>
+      <div class="footer-container">
+        <div class="footer-menu">
+          <ul>
+            <li><a href="Inicio.html">Inicio</a></li> 
+            <li><a href="ContenidoPrincipal/PoliticaPrivacidad.html" rel="noopener noreferrer">Política de privacidad</a></li>
+            <li><a href="ContenidoPrincipal/TerminosCondiciones.html" rel="noopener noreferrer">Términos y condiciones</a></li>
+          </ul>
+        </div>
+        <div class="footer-social">
+          <a href="https://www.tiktok.com/@estudianteprogramador" target="_blank" rel="noopener noreferrer"><img src="iconos/tik-tok.png" alt="TikTok" /></a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><img src="iconos/instagram.png" alt="Instagram" /></a>
+          <a href="https://www.patreon.com" target="_blank" rel="noopener noreferrer"><img src="iconos/patreon.png" alt="Patreon" /></a>
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"><img src="iconos/youtube.png" alt="YouTube" /></a>
+        </div>
+      </div>
+      <p>© 2024 Estudiante Programador - Todos los derechos reservados.</p>
+    </footer>
+
+    
