56ef4aa998fc4dcd9485aeda2e4b8abd12b6c579
javmunmay <javi15mmj@gmail.com>
2024-12-19 22:17:37 +0100

    Comprobaciones de conexion base de datos 2.2.7 (Beta)

--- php/conexion.php
+++ php/conexion.php
@@ -1,9 +1,9 @@
 <?php
 // Datos de conexión a la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "estudiante_programador";
+$servername = "PMYSQL181.dns-servicio.com:3306";
+$username = "Javier";
+$password = "u70q0Z2p@";
+$dbname = "10718674_estudiante_programador";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
