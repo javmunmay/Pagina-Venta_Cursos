6f84a3f3421ae62241ebc03c7efd21fc21bd4b6a
javmunmay <javi15mmj@gmail.com>
2025-03-08 00:32:10 +0100

    Error en la consola solucionado

--- ContenidoPrincipal/SobreNosotros.php
+++ ContenidoPrincipal/SobreNosotros.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre Nosotros | Estudiante Programador</title>
     <link rel="stylesheet" href="../css/sobreNosotros.css">
-    <link rel="stylesheet" href="../css/SobreNosotros.css?v=1.0">
+    <link rel="stylesheet" href="../css/sobreNosotros.css?v=1.0">
     <link rel="icon" type="image/png" href="../imagenes/favicon.ico">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
