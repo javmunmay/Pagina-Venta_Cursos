652fdf2caf1f797548708595309120030a18a7a9
javmunmay <javi15mmj@gmail.com>
2025-03-07 02:16:40 +0100

    pruebas

--- InfoCursosSesion/Curso.php
+++ InfoCursosSesion/Curso.php
@@ -12,7 +12,7 @@ header("Expires: 0"); // Proxies.
 // Verificar si el usuario ha iniciado sesión
 if (!isset($_SESSION['user_id'])) {
     // Redirigir al login si no está autenticado
-    header("Location: InicioSesion.php");
+    header("Location: ../InicioSesion/InicioSesion.php");
     exit();
 }
 
