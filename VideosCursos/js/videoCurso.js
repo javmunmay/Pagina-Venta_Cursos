3a7d06210cfb2c014426ddcb26e79be501ffbcd9
javmunmay <javi15mmj@gmail.com>
2025-03-07 22:20:25 +0100

    mejoras en el videoCurso totalmente responsive

--- VideosCursos/js/videoCurso.js
+++ VideosCursos/js/videoCurso.js
@@ -63,3 +63,33 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("El navegador bloqueó el autoplay:", error);
     });
 });
+
+
+
+
+
+//Menu desplegable header
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+  
+    // Alternar el menú al hacer clic en el botón
+    menuToggle.addEventListener("click", function (e) {
+      e.stopPropagation(); // Evitar que el clic se propague al documento
+      navLinks.classList.toggle("show");
+    });
+  
+    // Cerrar el menú al hacer clic fuera de él
+    document.addEventListener("click", function (e) {
+      if (!navLinks.contains(e.target) && !menuToggle.contains(e.target)) {
+        navLinks.classList.remove("show");
+      }
+    });
+  
+    // Cerrar el menú al hacer clic en un enlace
+    navLinks.querySelectorAll("a").forEach(link => {
+      link.addEventListener("click", () => {
+        navLinks.classList.remove("show");
+      });
+    });
+  });
